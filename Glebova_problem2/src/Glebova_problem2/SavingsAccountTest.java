e75648d6101901b8b7a376be30a6477e6290a735
Anastasia Glebova <aglebova@knights.ucf.edu>
2020-02-23 23:46:16 -0500

    made the test print all 12 months

--- Glebova_problem2/src/Glebova_problem2/SavingsAccountTest.java
+++ Glebova_problem2/src/Glebova_problem2/SavingsAccountTest.java
@@ -7,11 +7,19 @@ public class SavingsAccountTest {
 		SavingsAccount saver2 = new SavingsAccount(3000.0);
 		
 		SavingsAccount.modifyInterestRate(0.04);
-		System.out.printf("%.2f\n%.2f\n", saver1.calculateMonthlyInterest(), saver2.calculateMonthlyInterest());
+		System.out.println("Saver 1:                     Saver 2:");
+		for(int i = 0; i < 12; i++)
+		{
+			System.out.printf("Month %d. %.2f             Month %d. %.2f\n", i+1,saver1.calculateMonthlyInterest(), i+1, saver2.calculateMonthlyInterest());
+		}
+		
 		
 		SavingsAccount.modifyInterestRate(0.05);
-		System.out.printf("%.2f\n%.2f", saver1.calculateMonthlyInterest(), saver2.calculateMonthlyInterest());
-
+		System.out.println("\nSaver 1:                     Saver 2:");
+		for(int i = 0; i < 12; i++)
+		{
+			System.out.printf("Month %d. %.2f             Month %d. %.2f\n", i+1,saver1.calculateMonthlyInterest(), i+1, saver2.calculateMonthlyInterest());
+		}
 	}
 
 }
